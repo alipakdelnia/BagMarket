5d1cabf49ccc8edae42711e6fa8ff0eb43ab1f21
james <jamesrain1998@gmail.com>
2023-09-14 22:54:35 +0330

    navigation : all page added but all of them is empty

--- app/src/main/java/com/example/bagmarket/MainActivity.kt
+++ app/src/main/java/com/example/bagmarket/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.bagmarket
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.bagmarket.ui.theme.MainAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,17 +23,112 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BagMarketUi() {
 
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = ""
+    ) {
+
+        composable("mainScreen") {
+            MainScreen()
+        }
+
+        composable(
+            route = "productScreen",
+            arguments = listOf(navArgument("productId") {
+                type = NavType.IntType
+            })
+        ) {
+            ProductScreen(it.arguments!!.getInt("productId",-1))
+        }
+
+        composable(
+            route = "categoryScreen",
+        arguments = listOf(navArgument("categoryName"){
+            type  = NavType.StringType
+        })
+            ) {
+            CategoryScreen(it.arguments!!.getString("categoryName","null"))
+        }
+
+        composable("profileScreen") {
+            ProfileScreen()
+        }
+
+        composable("cartScreen") {
+            CartScreen()
+        }
+
+        composable("singUpScreen") {
+            SingUpScreen()
+        }
+
+        composable("singInScreen") {
+            SingInScreen()
+        }
+
+        composable("introScreen") {
+            IntroScreen()
+        }
+
+        composable("noInternetScreen") {
+            NoInternetScreen()
+        }
+
+
+    }
+
+
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NoInternetScreen() {
+
 }
+
+@Composable
+fun IntroScreen() {
+
+}
+
+@Composable
+fun SingInScreen() {
+
+}
+
+@Composable
+fun SingUpScreen() {
+
+}
+
+@Composable
+fun CartScreen() {
+
+}
+
+@Composable
+fun ProfileScreen() {
+
+}
+
+@Composable
+fun CategoryScreen(categoryName:String) {
+
+}
+
+@Composable
+fun ProductScreen(productId: Int) {
+
+}
+
+@Composable
+fun MainScreen() {
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MainAppTheme {
-        Greeting("Android")
-    }
+    MainAppTheme { BagMarketUi() }
 }
