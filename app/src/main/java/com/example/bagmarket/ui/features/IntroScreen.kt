de0683f695994ca19287efd8664ceabe43750a59
james <jamesrain1998@gmail.com>
2023-09-17 19:21:14 +0330

    my github added to IntroScreen.kt

--- app/src/main/java/com/example/bagmarket/ui/features/IntroScreen.kt
+++ app/src/main/java/com/example/bagmarket/ui/features/IntroScreen.kt
@@ -1,15 +1,23 @@
 package com.example.bagmarket.ui.features
 
+import android.content.Intent
+import android.net.Uri
+import android.widget.ImageButton
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import com.example.bagmarket.R
 import com.example.bagmarket.ui.theme.BackgroundMain
 import com.example.bagmarket.ui.theme.Blue
@@ -31,6 +39,8 @@ fun IntroScreenPreview() {
 @Composable
 fun IntroScreen() {
     val navigation = getNavController()
+    val uriHandler = LocalUriHandler.current
+
 
     Image(
         modifier = Modifier
@@ -65,5 +75,40 @@ fun IntroScreen() {
                 navigation.navigate(MyScreens.SignInScreen.route)
             }) { Text(text = "Sign In", color = Blue) }
 
+
     }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.95f),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Bottom){
+        Text(
+            text = "Created by",
+            modifier = Modifier
+                .rotate(23f)
+                .padding(end = 45.dp, bottom = 15.dp)
+            )
+        Text(
+            text = "Ali Pakdelnia",
+            modifier = Modifier
+                .rotate(23f)
+                .padding(end = 15.dp, bottom = 15.dp)
+            )
+        Image(
+            painter = painterResource(R.drawable.github_logo),
+            contentDescription =null,
+            modifier = Modifier
+                .rotate(23f)
+                .padding(end = 60.dp)
+                .size(85.dp,85.dp)
+                .clickable {
+                        uriHandler.openUri("https://github.com/alipakdelnia")
+            }
+
+        )
+    }
+
+
 }
