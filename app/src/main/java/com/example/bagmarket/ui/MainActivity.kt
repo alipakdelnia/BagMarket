bea9a5013d1a05b103edd7c93ba86673fb626d13
ali pakdelnia <jamesrain1998@gmail.com>
2023-10-10 13:29:28 +0330

    some bug fixed

--- app/src/main/java/com/example/bagmarket/ui/MainActivity.kt
+++ app/src/main/java/com/example/bagmarket/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bagmarket.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -33,6 +34,7 @@ import org.koin.android.ext.koin.androidContext
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR
         setContent {
             Koin(appDeclaration = {
                 androidContext(this@MainActivity)
